5a5d78a3701ce604518056904528a65b81b984b5
ganezasan <boss@cheekit.com>
2016-11-25 14:27:54 +0900

    add(Private): add logout

--- private-client/src/App.js
+++ private-client/src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fetch from 'isomorphic-fetch';
 
 class App extends Component {
+
+  handleLogout() {
+    fetch('/logout', {
+      credentials: 'same-origin',
+      method: 'POST',
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: {},
+    })
+    .then((res) => res.json())
+    .then((res) => {
+      if(res.success) {
+        document.location.href = res.redirect;
+      }
+    }).catch((err) => {
+      console.error('Fetch logout ERROR:', err);
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +35,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.handleLogout}>Logout</button>
       </div>
     );
   }
